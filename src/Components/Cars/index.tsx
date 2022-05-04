612d196f9e0c82296feb23955520c30c0d1f6684
Fabio Vitor <fabvitor2010@gmail.com>
2022-05-03 23:32:27 -0300

    fix top content home

--- src/Components/Cars/index.tsx
+++ src/Components/Cars/index.tsx
@@ -79,7 +79,7 @@ const Cars: React.FC = (props) => {
           </ul>
         </nav>
       </header>
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="max-w-2xl mx-auto py-8 px-4  sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
           Beta Version
         </h2>
